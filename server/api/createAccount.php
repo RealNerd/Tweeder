aaa3b6db6a19af31ad6fe946f1ac12f4409a6a49
Blake Schwendiman <blake.schwendiman@gmail.com>
2016-02-03 10:56:24 -0700

    Minor fix to create account

--- server/api/createAccount.php
+++ server/api/createAccount.php
@@ -1,7 +1,12 @@
 <?php
 	include('../shared/shared.php');
 
-	$message  = $_POST['message'];
+	$password = $_POST['password'];
 	$username = $_POST['username'];
 
+	if (createAccount($username, $password)) {
+		http_response_code(204);
+	} else {
+		http_response_code(409);
+	}
 ?>
